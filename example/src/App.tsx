ed45fb92f665850b190b0b15c3816f1d3d31ba9e
sqe-riza <riza.rahman@smma.id>
2024-01-09 16:22:18 +0700

    feat: init

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-android-liveness';
+import { multiply, sdkVersion } from 'react-native-android-liveness';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
     multiply(3, 7).then(setResult);
+    console.info(sdkVersion());
   }, []);
 
   return (
